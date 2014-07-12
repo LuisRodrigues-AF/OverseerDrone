8d42d89b4f8340e80446fdaaba5cb58b20fbe6ad
Stuart Gilbert <stuart.gilbert@gmail.com>
2014-07-12 13:28:22 +0100

    Adding in an extremely basic client page.
    The page auto-polls for location every 5 seconds and attempts
    to push it to a server. It also has very basic tracking of
    time since the alst successful location push.

---
+++ src/www/client/locationProvider.js
@@ -0,0 +1,36 @@
+var LOCATION_RECEIVER_URL = "http://stu.ie/AnalogFolk/locationReceiver.cgi";
+
+var lastSuccessfulLocationUpdateTime = 0;
+
+function updateLocation() {
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(pushCurrentLocation);
+    } else {
+        alert("Your shitty browser doesn't support geolocation. Find your friends yourself, caveman.");
+    }
+}
+
+function pushCurrentLocation(position) {
+    var positionData = {
+        userId: "1",
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+    };
+
+    jQuery.getJSON(LOCATION_RECEIVER_URL, positionData)
+        .done(function(data) {
+            console.log("Posted location");
+            lastSuccessfulLocationUpdateTime = (new Date).getTime();
+        })
+        .fail(function() {
+            console.log("Failed to post location");
+            var currentTime = (new Date).getTime();
+
+            if(lastSuccessfulLocationUpdateTime > 0 && currentTime - lastSuccessfulLocationUpdateTime > 10000) {
+                // It's been more than 10 seconds since the last successful location update.
+                // Give some indication that your location data is a bit out of date.
+            }
+        });
+}
+
+setInterval(function() {updateLocation();}, 5000);
